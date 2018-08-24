0a10376311a431bbda8601b3c04d9b48b9148020
patryk-kochanski <patryk.kochanski@gmail.com>
2018-08-24 21:32:31 +0200

    Add authentication

--- app.js
+++ app.js
@@ -2,15 +2,36 @@ const express = require('express');
 const path = require ('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const passport = require('passport-jwt');
+const passport = require('passport');
 const mongoose = require('mongoose');
-
+const users = require('./routes/users');
+const config = require('./config/database');
 const app = express();
 const port = 8080;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/users', users);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+
+//Connect to DB
+mongoose.connect(config.database, {useNewUrlParser: true});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB: '+ config.database);
+})
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: '+ err);
+})
 
 app.get('/', (req, res) => {
     res.send('Hello world! ');
 })
+
+
+//Start server
 app.listen(port, ()=> {
     console.log('Server started on port ' + port + ' Press CTRL+C to exit. ')
 });
